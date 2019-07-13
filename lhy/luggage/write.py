7b9f43f2f9317eb7c54c9aa81e131977b92d0ca7
lihaoyang1013 <2013867577@qq.com>
2019-07-13 19:24:17 +0800

    箱包

---
+++ lhy/luggage/write.py
@@ -0,0 +1,11 @@
+import os
+
+#写入txt文件
+datapath = r'D:\tupian\val'
+file = open('val.txt', 'w')
+for dir in sorted(os.listdir(datapath)):
+    path = (os.path.join(datapath, dir))
+    for name in os.listdir(path):
+        a = int(path[-4:])
+        file.write (os.path.join(path, name)+"  "+str(a)+"\n")
+        file.flush()
