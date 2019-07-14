514e252b732f0ac81b37f15713f932d9ec1e4ee2
lihaoyang1013 <2013867577@qq.com>
2019-07-14 16:41:23 +0800

    箱包

--- lhy/luggage/classification.py
+++ lhy/luggage/classification.py
@@ -13,6 +13,6 @@ for dir_name in list_path1:
     if move > 0 and (not os.path.exists(os.path.join(path2, dir_name))):
         os.makedirs(os.path.join(path2, dir_name))
     for j in range(move):
-        x = random.randint(0, len(file))
+        x = random.randint(0, （len(file)-1）)
         shutil.move(os.path.join(path1, dir_name, file[x]),os.path.join(path2, dir_name, file[x]))
         file.remove(file[x])
