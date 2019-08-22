a385138755224d899e1dd90c6fda42be476c9cab
yangli <yl99@163.com>
2019-08-22 20:37:12 +0800

    Added      yl/PyTorch-YOLOv3-master/mine/compose_sample.py Added      yl/PyTorch-YOLOv3-master/mine/gen_sample_fixsize.py Added      yl/PyTorch-YOLOv3-master/mine/readme.md

---
+++ yl/PyTorch-YOLOv3-master/mine/compose_sample.py
@@ -0,0 +1,55 @@
+
+import cv2
+import os
+import numpy as np
+
+
+if __name__ == '__main__':
+    fgDir = r'F:\data\mine\web_fixsize' #前景图
+    bgDir = r'F:\data\uav\neg' # 背景图目录
+    trainImgDir = r'F:\data\mine\train' # 训练图
+    trainLabelDir = r'F:\data\mine\train_label' # 训练标签
+    trainSize = 416 #输出的训练图像大小
+
+    os.makedirs(trainImgDir, exist_ok=True)
+    os.makedirs(trainLabelDir, exist_ok=True)
+
+    fgFiles = []
+    for file in os.listdir(fgDir):
+        _, ext = os.path.splitext(file)
+        if ext == '.jpg':
+            fgFiles.append(file)
+
+    fgIndex = 0
+    for file in os.listdir(bgDir):
+        print(file)
+        img = cv2.imread(os.path.join(bgDir, file))
+        if img is None:
+            print('%s does not exist' % file)
+            continue
+        h, w, c = img.shape
+        step = (trainSize * 3) // 4
+        # 遍历背景图
+        for row in range(0, h - trainSize, step):
+            for col in range(0, w - trainSize, step):
+                bgImg = img[row:row + trainSize, col:col + trainSize, :]
+                fgImg = cv2.imread(os.path.join(fgDir, fgFiles[fgIndex]))
+                if bgImg.shape[0] - fgImg.shape[0] < 0 or bgImg.shape[1] - fgImg.shape[1] < 0:
+                    print('%s is too large' % fgFiles[fgIndex])
+                    continue
+                y1 = (bgImg.shape[0] - fgImg.shape[0]) // 2
+                x1 = (bgImg.shape[1] - fgImg.shape[1]) // 2
+                bgImg[y1:y1+fgImg.shape[0], x1:x1+fgImg.shape[1], :] = fgImg # 两图叠加
+                name = os.path.join(trainImgDir, '%s_%d_%d_%s' %(file, row, col, fgFiles[fgIndex]))
+                cv2.imwrite(name, bgImg)
+
+                filename, ext = os.path.splitext(fgFiles[fgIndex])
+                flabel = open(os.path.join(fgDir, filename + '.txt'), 'r')
+                annot = flabel.readline().strip().split(' ')
+                outFile = open(os.path.join(trainLabelDir, os.path.splitext(name)[0] + '.txt'), 'w')
+                cx = (float(annot[1]) + float(annot[3])) / 2 + x1
+                cy = (float(annot[2]) + float(annot[4])) / 2 + y1
+                width = float(annot[3]) - float(annot[1]) + 1
+                height = float(annot[4]) - float(annot[2]) + 1
+                outFile.write('0 %f %f %f %f\n' % (cx / bgImg.shape[1], cy / bgImg.shape[0], width / bgImg.shape[1], height / bgImg.shape[0]))
+                fgIndex = (fgIndex + 1) % len(fgFiles)
---
+++ yl/PyTorch-YOLOv3-master/mine/gen_sample_fixsize.py
@@ -0,0 +1,88 @@
+
+import json
+import cv2
+import os
+import numpy as np
+import ellipses
+import random
+
+if __name__ == '__main__':
+    root = r'F:\data\mine\web' #原图
+    outDir = r'F:\data\mine\web_fixsize' #前景图
+    bgDir = r'F:\data\uav\neg' # 背景图目录
+    trainDir = r'F:\data\mine\train' # 训练图
+    fixsize = [16, 32] # 目标大小范围
+    trainSize = 416 #输出的训练图像大小
+
+    os.makedirs(outDir, exist_ok=True)
+    os.makedirs(trainDir, exist_ok=True)
+
+    show_scale = 1
+    for file in os.listdir(root):
+        filename, ext = os.path.splitext(file)
+        if ext != '.json':
+            continue
+        print(file)
+        with open(os.path.join(root, file),'r') as load_f:
+            load_dict = json.load(load_f)
+        img = cv2.imread(os.path.join(root, load_dict["imagePath"]))
+        if img is None:
+            print('image file does not exist')
+        ih, iw, ic = img.shape
+        smallImg = cv2.resize(img, (int(img.shape[1]/ show_scale), int(img.shape[0]/ show_scale)))
+        objectNum = len(load_dict['shapes'])
+        for i, shape in enumerate(load_dict['shapes']):
+            pts = np.array(shape['points'])
+
+            if pts.shape[0] >= 6: # circle
+                n = pts.shape[0]
+                data = [pts[0:n-1, 0], pts[0:n-1, 1]]
+                lsqe = ellipses.LSqEllipse()
+                lsqe.fit(data)
+                center, width, height, phi = lsqe.parameters()
+                cv2.ellipse(smallImg, (int(center[0]), int(center[1])), (int(width), int(height)), phi / 3.14159 * 180,
+                            0, 360, (255, 0, 0), 2)
+                ellpts = cv2.ellipse2Poly((int(center[0]), int(center[1])), (int(width), int(height)), int(phi / 3.14159 * 180),
+                            0, 360, 5)
+                ellpts = np.append(ellpts, [[pts[n-1, 0], pts[n-1, 1]]], axis=0)
+                x2, y2 = np.max(ellpts, 0)
+                x1, y1 = np.min(ellpts, 0)
+
+            else: # rectangle
+                x2, y2 = np.max(pts, 0)
+                x1, y1 = np.min(pts, 0)
+
+            x1 = max(x1, 0)
+            y1 = max(y1, 0)
+            x2 = min(x2, iw - 1)
+            y2 = min(y2, ih - 1)
+
+            intpt1 = (int(int(x1) / show_scale), int(int(y1) / show_scale))
+            intpt2 = (int(int(x2) / show_scale), int(int(y2) / show_scale))
+            cv2.rectangle(smallImg, intpt1, intpt2, (0, 0, 255))
+
+            size1 = x2 - x1 + 1
+            size = random.randint(fixsize[0], fixsize[1])
+            r = size / size1
+            outImg = cv2.resize(img, (int(img.shape[1] * r), int(img.shape[0] * r)))
+            x1 = int(x1 * r)
+            x2 = int(x2 * r)
+            y1 = int(y1 * r)
+            y2 = int(y2 * r)
+            name_ = os.path.join(outDir, '%s_%d.jpg' % (filename, i))
+            outTxt = open(os.path.join(outDir, '%s_%d.txt' % (filename, i)), 'w')
+            if objectNum == 1: # 只有一个物体，存整张图
+                outTxt.write('0 %f %f %f %f\n' % (x1, y1, x2, y2))
+                cv2.imwrite(name_, outImg)
+            else: # 有多个物体，存为多个文件
+                m = random.randint(2, 6)
+                x11 = max(x1 - m, 0)
+                x22 = min(x2 + m, outImg.shape[1] - 1)
+                y11 = max(y1 - m, 0)
+                y22 = min(y2 + m, outImg.shape[0] - 1)
+                cv2.imwrite(name_, outImg[y11:y22+1, x11:x22+1, :])
+                outTxt.write('0 %f %f %f %f\n' % (x1 - x11, y1 - y11, x2 - x11, y2 - y11))
+            outTxt.close()
+            #out.write('0 %f %f %f %f\n' % ((x1 + x2) * 0.5 / iw, (y1 + y2) * 0.5 / ih, (x2 - x1 + 1) / iw, (y2 - y1 + 1) / ih))
+        cv2.imshow('image', smallImg)
+        cv2.waitKey(1)
